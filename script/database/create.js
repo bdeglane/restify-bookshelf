f903c25f690e4d6394cb814d098555b5d11e919c
B D <bdeglane@users.noreply.github.com>
2017-01-18 11:08:53 +0100

    script to create database schema

--- script/database/create.js
+++ script/database/create.js
@@ -1,15 +1,28 @@
 const debug = require('debug')('/script/database/create.js');
 const logger = require('winston');
+const readline = require('readline');
 
 const orm = require('../../conf/config-orm');
 const schema = require('../../src/app/resource/schema');
 
-if (process.env.NODE_ENV == 'development') {
-  schema.createSchema(orm.knex, function (err) {
-    if (err) {
-      process.exit(1);
-    }
-    knex.destroy();
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question(`your NODE_ENV is ${process.env.NODE_ENV}\nWould you drop and create a new database schema ? (y/n) `, (answer) => {
+
+  if (answer.match(/^y(es)?$/i)) {
+    schema.createSchema(orm.knex, function (err) {
+      if (err) {
+        process.exit(1);
+      }
+      knex.destroy();
+      process.exit(0);
+    });
+  } else {
+    console.log(`Doing nothing`);
     process.exit(0);
-  });
-}
+  }
+  rl.close();
+});
